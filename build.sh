caa7f25a6a6e5a71f77984e0936dcbea2466fc99
Takoua Jelassi <takoua.jelassi@aislab.de>
2026-04-22 11:30:02 +0200

    admin login

--- build.sh
+++ build.sh
@@ -10,12 +10,16 @@ import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+fullname = os.environ.get('DJANGO_SUPERUSER_FULLNAME', 'Admin User')
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        fullname=fullname,
+        password=password
+    )
     print("Superuser created")
 else:
     print("Superuser already exists")
