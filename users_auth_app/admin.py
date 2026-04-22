42bbeaee5d2f830ed368a318861fc3497ee26f4a
Takoua Jelassi <takoua.jelassi@aislab.de>
2026-04-22 12:48:56 +0200

    update admin

--- users_auth_app/admin.py
+++ users_auth_app/admin.py
@@ -1,29 +1,22 @@
 from django.contrib import admin
+
+# Register your models here.
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 from django.utils.translation import gettext_lazy as _
 
-from .models import User
-from .forms import UserCreationForm, UserChangeForm
-
-
-@admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-
     ordering = ["email"]
-    list_display = ["id", "email", "fullname", "is_staff", "is_active"]
+    list_display = ["id","email", "fullname", "is_staff", "is_active"]
     list_filter = ["is_staff", "is_active", "is_superuser"]
-
+    
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Personal info"), {"fields": ("fullname",)}),
-        (_("Permissions"), {
-            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
-        }),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
-
+    
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
@@ -33,3 +26,5 @@ class UserAdmin(BaseUserAdmin):
 
     search_fields = ("email", "fullname")
     filter_horizontal = ("groups", "user_permissions")
+
+admin.site.register(User, UserAdmin)
